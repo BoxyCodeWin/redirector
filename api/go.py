e872a1e1adf5ae35633a334c1e2c8f0b448bbb23
Your Name <you@example.com>
2025-10-29 16:01:25 +0900

    feat: support HEAD for vercel

--- api/go.py
+++ api/go.py
@@ -1,10 +1,15 @@
 import os
 from http.server import BaseHTTPRequestHandler
+
 class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        dest=os.environ.get("DEST")
-        if not dest: self.send_response(503); self.end_headers(); return
+    def _redirect(self):
+        dest = os.environ.get("DEST")
+        if not dest:
+            self.send_response(503); self.end_headers(); return
         self.send_response(302)
         self.send_header("Location", dest)
         self.send_header("Cache-Control", "no-store")
         self.end_headers()
+
+    def do_GET(self):  self._redirect()
+    def do_HEAD(self): self._redirect()
